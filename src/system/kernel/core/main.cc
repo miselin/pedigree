63756f5f7ba3b541f423a5fe1219e6d19fb0978d
Matthew Iselin <matthew@theiselins.net>
2016-05-07 10:02:06 -0700

    kernel: clean up KernelElf in system_reset

--- src/system/kernel/core/main.cc
+++ src/system/kernel/core/main.cc
@@ -441,6 +441,7 @@ void system_reset()
     KernelElf::instance().unloadModules();
     NOTICE("All modules unloaded. Running destructors and terminating...");
     runKernelDestructors();
+    KernelElf::instance().~KernelElf();
     Processor::deinitialise();
     Processor::reset();
     while(1);
