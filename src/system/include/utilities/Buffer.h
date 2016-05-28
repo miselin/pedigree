4add6d928ca37f29a72eba8d03dab1456ffb5e06
Matthew Iselin <matthew@theiselins.net>
2016-05-28 23:55:42 +1000

    utilities: add a new class Buffer<T> to replace RingBuffer<T>
    This uses much the same implementation as TcpBuffer, but is more general-purpose (it may actually not be a good fit to replace TcpBuffer, for example).

---
+++ src/system/include/utilities/Buffer.h
@@ -0,0 +1,207 @@
+/*
+ * Copyright (c) 2008-2014, Pedigree Developers
+ *
+ * Please see the CONTRIB file in the root of the source tree for a full
+ * list of contributors.
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifndef KERNEL_UTILITIES_BUFFER_H
+#define KERNEL_UTILITIES_BUFFER_H
+
+#include <process/Mutex.h>
+#include <process/ConditionVariable.h>
+
+class Thread;
+class Event;
+
+/**
+ * Provides a buffer of a specific size and utility functions for integration
+ * with e.g. File or other kernel systems.
+ *
+ * allowShortOperation defines the action to take on overflow. If true, the
+ * buffer's write() operation is permitted to return a size less than requested
+ * if the buffer would overflow. Otherwise, the implementation is required to
+ * block until bytes are present (unless blocking has been explicitly denied).
+ *
+ * Note that an attempt to read when writing is disabled that would block will
+ * always return the number of bytes read so far (or zero if none yet). The
+ * same is true for an attempt to write when reading is disabled if that would
+ * block.
+ */
+template <class T, bool allowShortOperation = false>
+class Buffer
+{
+    public:
+        Buffer(size_t bufferSize);
+        virtual ~Buffer();
+
+        /**
+         * Write \param count values from \param buffer, optionally blocking
+         * before writing if there is insufficient space.
+         */
+        size_t write(const T *buffer, size_t count, bool block = true);
+
+        /**
+         * Read \param count values into \param buffer, optionally blocking
+         * if no more values are available to be read yet.
+         */
+        size_t read(T *buffer, size_t count, bool block = true);
+
+        /**
+         * Disable further writes to the buffer.
+         * This will wake up all readers waiting on a writer.
+         */
+        void disableWrites();
+
+        /**
+         * Disable further reads from the buffer.
+         * This will wake up all writers waiting on reader.
+         */
+        void disableReads();
+
+        /**
+         * Enable writes to the buffer.
+         *
+         * \return the previous state of writes.
+         */
+        bool enableWrites();
+
+        /**
+         * Enable reads from the buffer.
+         *
+         * \return the previous state of reads.
+         */
+        bool enableReads();
+
+        /**
+         * Get the number of bytes in the buffer now.
+         */
+        size_t getDataSize();
+
+        /**
+         * Get the full size of the buffer (potential storage).
+         */
+        size_t getSize();
+
+        /**
+         * Check if the buffer can be written to.
+         * \note This does not guarantee the next write() will succeed.
+         */
+        bool canWrite(bool block);
+
+        /**
+         * Check if the buffer can be read from.
+         */
+        bool canRead(bool block);
+
+        /**
+         * Wipes the buffer.
+         */
+        void wipe();
+
+        /**
+         * Add an event to be sent to the given thread upon a data change.
+         *
+         * \note An event does not guarantee the next operation will succeed.
+         */
+        void monitor(Thread *pThread, Event *pEvent);
+
+        /**
+         * Remove monitoring targets for the given thread.
+         */
+        void cullMonitorTargets(Thread *pThread);
+
+    private:
+        WITHOUT_IMPLICIT_CONSTRUCTORS(Buffer);
+
+        /**
+         * Helper function to send events upon completing an action.
+         *
+         * Clears all monitors as a side effect.
+         */
+        void notifyMonitors();
+
+        /**
+         * Create a new segment with the given data.
+         */
+        void addSegment(const T *buffer, size_t count);
+
+        /**
+         * Controls the size of each segment.
+         */
+        static const size_t m_SegmentSize = 32768;
+
+        /**
+         * Holds a segment of data; more data can be written into this segment
+         * until it reaches capacity.
+         */
+        struct Segment
+        {
+            Segment() : data(), reader(0), size(0)
+            {
+            }
+
+            /** Segment data. */
+            T data[m_SegmentSize];
+
+            /** Reader offset (the next reader starts here). */
+            size_t reader;
+
+            /** Segment size so far. */
+            size_t size;
+        };
+
+        /**
+         * Contains information about a particular target to send events to.
+         */
+        struct MonitorTarget
+        {
+            MonitorTarget() : pThread(0), pEvent(0)
+            {
+            }
+
+            MonitorTarget(Thread *thread, Event *event) :
+                pThread(thread), pEvent(event)
+            {
+            }
+
+            Thread *pThread;
+            Event *pEvent;
+        };
+
+        size_t m_BufferSize;
+        size_t m_DataSize;
+
+        Mutex m_Lock;
+
+        ConditionVariable m_WriteCondition;
+        ConditionVariable m_ReadCondition;
+
+        List<Segment *> m_Segments;
+        List<MonitorTarget *> m_MonitorTargets;
+
+        bool m_bCanRead;
+        bool m_bCanWrite;
+};
+
+// Specializations are in a .cc file.
+extern template class Buffer<uint8_t, false>;
+extern template class Buffer<uint8_t, true>;
+extern template class Buffer<char, false>;
+extern template class Buffer<char, true>;
+extern template class Buffer<size_t, false>;
+extern template class Buffer<size_t, true>;
+
+#endif  // KERNEL_UTILITIES_BUFFER_H
---
+++ src/system/kernel/utilities/Buffer.cc
@@ -0,0 +1,467 @@
+/*
+ * Copyright (c) 2008-2014, Pedigree Developers
+ *
+ * Please see the CONTRIB file in the root of the source tree for a full
+ * list of contributors.
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <utilities/Buffer.h>
+#include <utilities/utility.h>
+#include <LockGuard.h>
+#include <Log.h>
+
+#ifdef THREADS
+#include <process/Thread.h>
+#endif
+
+template <class T, bool allowShortOperation>
+Buffer<T, allowShortOperation>::Buffer(size_t bufferSize) :
+    m_BufferSize(bufferSize), m_DataSize(0), m_Lock(false), m_WriteCondition(),
+    m_ReadCondition(), m_Segments(), m_MonitorTargets(), m_bCanRead(true),
+    m_bCanWrite(true)
+{
+}
+
+template <class T, bool allowShortOperation>
+Buffer<T, allowShortOperation>::~Buffer()
+{
+    // Wake up all readers and writers to finish up existing operations.
+    disableReads();
+    disableWrites();
+
+    // Clean up the entire buffer.
+    wipe();
+
+    // Clean up monitor targets.
+    m_Lock.acquire();
+    for (auto pTarget : m_MonitorTargets)
+    {
+        delete pTarget;
+    }
+    m_MonitorTargets.clear();
+    m_Lock.release();
+}
+
+template <class T, bool allowShortOperation>
+size_t Buffer<T, allowShortOperation>::write(const T *buffer, size_t count, bool block)
+{
+    m_Lock.acquire();
+
+    size_t countSoFar = 0;
+    while (true)
+    {
+        // Can we write?
+        if (!m_bCanWrite)
+        {
+            // No! Maybe not anymore, so return what we've written so far.
+            break;
+        }
+
+        // Do we have space?
+        size_t bytesAvailable = m_BufferSize - m_DataSize;
+        if (!bytesAvailable)
+        {
+            if (!block)
+            {
+                // Cannot block!
+                break;
+            }
+
+            // Can any reader get us out of this situation?
+            if (!m_bCanRead)
+            {
+                // No. Return what we've written so far.
+                break;
+            }
+
+            // No, we need to wait.
+            NOTICE("BUFFER: block on WRITE");
+            while (!m_WriteCondition.wait(m_Lock))
+                ;
+            continue;
+        }
+
+        // Yes, we have room.
+        size_t totalCount = bytesAvailable;
+        if (totalCount > count)
+        {
+            totalCount = count;
+        }
+
+        // If we're allowed to just give up if we don't have enough bytes, do
+        // so (e.g. for TCP buffers and the like).
+        if (allowShortOperation && (totalCount > bytesAvailable))
+        {
+            totalCount = bytesAvailable;
+            count = bytesAvailable;
+            if (!totalCount)
+            {
+                break;
+            }
+        }
+
+        // Add the segment.
+        bool copiedData = false;
+        while (totalCount)
+        {
+            size_t numberCopied = 0;
+            if (totalCount >= m_SegmentSize)
+            {
+                // Ignore any existing segments, just create a new one.
+                addSegment(buffer, m_SegmentSize);
+                numberCopied = m_SegmentSize;
+            }
+            else if (!m_Segments.count())
+            {
+                // Just add this segment.
+                addSegment(buffer, totalCount);
+                numberCopied = totalCount;
+            }
+            else
+            {
+                // Can we modify the most recent segment?
+                Segment *pSegment = m_Segments.popBack();
+                if (pSegment->size == m_SegmentSize)
+                {
+                    m_Segments.pushBack(pSegment);
+
+                    // Full already, need a new segment.
+                    addSegment(buffer, totalCount);
+                    numberCopied = totalCount;
+                }
+                else
+                {
+                    // There's room in this segment.
+                    T *start = &pSegment->data[pSegment->size];
+                    size_t availableSpace = m_SegmentSize - pSegment->size;
+                    if (availableSpace > totalCount)
+                    {
+                        availableSpace = totalCount;
+                    }
+                    pedigree_std::copy(start, buffer, availableSpace);
+
+                    // We just added more bytes to this segment.
+                    pSegment->size += availableSpace;
+
+                    // Done with this segment, so push it back for reading.
+                    m_Segments.pushBack(pSegment);
+
+                    // Was that enough?
+                    if (availableSpace < totalCount)
+                    {
+                        // No, need one more segment.
+                        addSegment(&buffer[availableSpace], totalCount - availableSpace);
+                    }
+
+                    // Done.
+                    numberCopied = totalCount;
+                }
+            }
+
+            countSoFar += numberCopied;
+            m_DataSize += numberCopied;
+            buffer += numberCopied;
+            totalCount -= numberCopied;
+            count -= numberCopied;
+
+            if (!copiedData)
+            {
+                copiedData = numberCopied > 0;
+            }
+        }
+
+        // Wake up a reader that was waiting for data.
+        // We do this here rather than after we finish as we may need to block
+        // again (e.g. if we're writing more than the size of the buffer), and
+        // a reader is needed to unblock that.
+        if (copiedData)
+        {
+            NOTICE("BUFFER: signal READER");
+            m_ReadCondition.signal();
+        }
+
+        if (!count)
+        {
+            // Complete.
+            break;
+        }
+    }
+
+    m_Lock.release();
+
+    if (countSoFar)
+    {
+        // We've updated the buffer, so send events.
+        notifyMonitors();
+    }
+
+    return countSoFar;
+}
+
+template <class T, bool allowShortOperation>
+size_t Buffer<T, allowShortOperation>::read(T *buffer, size_t count, bool block)
+{
+    m_Lock.acquire();
+
+    size_t countSoFar = 0;
+    while (true)
+    {
+        // Can we read?
+        if (!m_bCanRead)
+        {
+            // No! Maybe not anymore, so return what we've read so far.
+            break;
+        }
+
+        // Do we have anything to read?
+        if (!m_DataSize)
+        {
+            if (!block)
+            {
+                // Cannot block!
+                break;
+            }
+
+            // Can any writer get us out of this situation?
+            if (!m_bCanWrite)
+            {
+                // No. Return what we've read so far.
+                break;
+            }
+
+            // No, we need to wait.
+            NOTICE("BUFFER: block on READ");
+            while (!m_ReadCondition.wait(m_Lock))
+                ;
+            continue;
+        }
+
+        // Yes, we have room.
+        size_t totalCount = count;
+        if (totalCount > m_DataSize)
+        {
+            totalCount = m_DataSize;
+        }
+
+        size_t numberCopied = 0;
+        while (m_Segments.count() && numberCopied < totalCount)
+        {
+            // Grab the first segment and read it.
+            Segment *pSegment = m_Segments.popFront();
+            size_t countToRead = pSegment->size - pSegment->reader;
+            if ((numberCopied + countToRead) > totalCount)
+            {
+                countToRead = totalCount - numberCopied;
+            }
+
+            // Copy.
+            pedigree_std::copy(buffer, &pSegment->data[pSegment->reader], countToRead);
+            pSegment->reader += countToRead;
+
+            // Do we need to re-add it?
+            if (pSegment->reader < pSegment->size)
+            {
+                m_Segments.pushFront(pSegment);
+            }
+            else
+            {
+                delete pSegment;
+            }
+
+            numberCopied += countToRead;
+            buffer += countToRead;
+        }
+
+        m_DataSize -= numberCopied;
+        countSoFar += numberCopied;
+        count -= numberCopied;
+
+        // We read some bytes so writers may be able to continue, which may be
+        // needed to unblock us if we loop back around and block.
+        if (numberCopied)
+        {
+            // Wake up a writer that was waiting for space to write.
+            NOTICE("BUFFER: signal READER");
+            m_WriteCondition.signal();
+        }
+
+        if (!count)
+        {
+            break;
+        }
+
+        // Once we've read at least some bytes, don't block - just return what
+        // we've read so far if we loop back around and have no data.
+        block = false;
+    }
+
+    m_Lock.release();
+
+    if (countSoFar)
+    {
+        // We've updated the buffer, so send events.
+        notifyMonitors();
+    }
+
+    return countSoFar;
+}
+
+template <class T, bool allowShortOperation>
+void Buffer<T, allowShortOperation>::disableWrites()
+{
+    LockGuard<Mutex> guard(m_Lock);
+    m_bCanWrite = false;
+
+    // All pending readers need to now return.
+    m_ReadCondition.broadcast();
+}
+
+template <class T, bool allowShortOperation>
+void Buffer<T, allowShortOperation>::disableReads()
+{
+    LockGuard<Mutex> guard(m_Lock);
+    m_bCanRead = false;
+
+    // All pending writers need to now return.
+    m_WriteCondition.broadcast();
+}
+
+template <class T, bool allowShortOperation>
+bool Buffer<T, allowShortOperation>::enableWrites()
+{
+    LockGuard<Mutex> guard(m_Lock);
+    bool previous = m_bCanWrite;
+    m_bCanWrite = true;
+    return previous;
+}
+
+template <class T, bool allowShortOperation>
+bool Buffer<T, allowShortOperation>::enableReads()
+{
+    LockGuard<Mutex> guard(m_Lock);
+    bool previous = m_bCanRead;
+    m_bCanRead = true;
+    return previous;
+}
+
+template <class T, bool allowShortOperation>
+size_t Buffer<T, allowShortOperation>::getDataSize()
+{
+    LockGuard<Mutex> guard(m_Lock);
+    return m_DataSize;
+}
+
+template <class T, bool allowShortOperation>
+size_t Buffer<T, allowShortOperation>::getSize()
+{
+    return m_BufferSize;
+}
+
+template <class T, bool allowShortOperation>
+bool Buffer<T, allowShortOperation>::canWrite(bool block)
+{
+    LockGuard<Mutex> guard(m_Lock);
+    return m_bCanWrite;
+}
+
+template <class T, bool allowShortOperation>
+bool Buffer<T, allowShortOperation>::canRead(bool block)
+{
+    LockGuard<Mutex> guard(m_Lock);
+    return m_bCanRead;
+}
+
+template <class T, bool allowShortOperation>
+void Buffer<T, allowShortOperation>::wipe()
+{
+    LockGuard<Mutex> guard(m_Lock);
+
+    // Wipe out every segment we own.
+    for (auto pSegment : m_Segments)
+    {
+        delete pSegment;
+    }
+    m_Segments.clear();
+    m_DataSize = 0;
+
+    // Notify writers that might have been waiting for space.
+    m_WriteCondition.signal();
+}
+
+template <class T, bool allowShortOperation>
+void Buffer<T, allowShortOperation>::monitor(Thread *pThread, Event *pEvent)
+{
+#ifdef THREADS
+    LockGuard<Mutex> guard(m_Lock);
+    MonitorTarget *pTarget = new MonitorTarget(pThread, pEvent);
+    m_MonitorTargets.pushBack(pTarget);
+#endif
+}
+
+template <class T, bool allowShortOperation>
+void Buffer<T, allowShortOperation>::cullMonitorTargets(Thread *pThread)
+{
+#ifdef THREADS
+    LockGuard<Mutex> guard(m_Lock);
+    for (auto it = m_MonitorTargets.begin(); it != m_MonitorTargets.end(); ++it)
+    {
+        MonitorTarget *pMT = *it;
+
+        if (pMT->pThread == pThread)
+        {
+            delete pMT;
+            m_MonitorTargets.erase(it);
+            it = m_MonitorTargets.begin();
+            if (it == m_MonitorTargets.end())
+                return;
+        }
+    }
+#endif
+}
+
+template <class T, bool allowShortOperation>
+void Buffer<T, allowShortOperation>::notifyMonitors()
+{
+#ifdef THREADS
+    LockGuard<Mutex> guard(m_Lock);
+    for (typename List<MonitorTarget*>::Iterator it = m_MonitorTargets.begin();
+            it != m_MonitorTargets.end();
+            it++)
+    {
+        MonitorTarget *pMT = *it;
+
+        pMT->pThread->sendEvent(pMT->pEvent);
+        delete pMT;
+    }
+    m_MonitorTargets.clear();
+#endif
+}
+
+template <class T, bool allowShortOperation>
+void Buffer<T, allowShortOperation>::addSegment(const T *buffer, size_t count)
+{
+    // Called with lock taken.
+    Segment *pNewSegment = new Segment();
+    pedigree_std::copy(pNewSegment->data, buffer, count);
+    pNewSegment->size = count;
+    m_Segments.pushBack(pNewSegment);
+}
+
+template class Buffer<uint8_t, false>;
+template class Buffer<uint8_t, true>;
+template class Buffer<char, false>;
+template class Buffer<char, true>;
+template class Buffer<size_t, false>;
+template class Buffer<size_t, true>;
