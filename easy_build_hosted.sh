10018acb8b87769295d9bc191d1fc0c4528af808
Matthew Iselin <matthew@theiselins.net>
2016-11-03 14:25:03 -0700

    build: remove libbind from hosted builds too

--- easy_build_hosted.sh
+++ easy_build_hosted.sh
@@ -47,7 +47,6 @@ $script_dir/run_pup.py sync
 
 # Needed for libc
 $script_dir/run_pup.py install ncurses
-$script_dir/run_pup.py install libbind
 
 # Run a quick build of libc and libm for the rest of the build system.
 scons hosted=1 CROSS=$script_dir/compilers/dir/bin/x86_64-pedigree- build/musl/lib/libc.so
