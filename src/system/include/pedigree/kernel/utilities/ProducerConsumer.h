069faa4bd09ea67fcc5771cd14022b826ada03e6
Matthew Iselin <matthew@theiselins.net>
2017-08-04 16:26:49 -0700

    utilities: add a producer/consumer queue
    This is a much lighter abstraction than RequestQueue as it doesn't need to handle things like passing up results from completed tasks. This makes it useful for use in environments where a work queue is needed but results and job status don't matter.

---
+++ src/system/include/pedigree/kernel/utilities/ProducerConsumer.h
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2008-2014, Pedigree Developers
+ *
+ * Please see the CONTRIB file in the root of the source tree for a full
+ * list of contributors.
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifndef KERNEL_UTILITIES_PRODUCERCONSUMER_H
+#define KERNEL_UTILITIES_PRODUCERCONSUMER_H
+
+#if defined(THREADS) || defined(UTILITY_LINUX)
+#define PRODUCERCONSUMER_ASYNCHRONOUS 1
+#else
+#define PRODUCERCONSUMER_ASYNCHRONOUS 0
+#endif
+
+#if PRODUCERCONSUMER_ASYNCHRONOUS
+#include "pedigree/kernel/process/ConditionVariable.h"
+#include "pedigree/kernel/process/Mutex.h"
+#include "pedigree/kernel/processor/types.h"
+#include "pedigree/kernel/utilities/List.h"
+#endif
+
+/**
+ * ProducerConsumer provides a very light abstraction for a work queue.
+ *
+ * Producer(s) push tasks into the queue by calling produce(). All of these
+ * work items are fire-and-forget - there is no way for callers to collect a
+ * return status or any other kind of information about the completion of a
+ * task. If this is important, a RequestQueue is a better choice.
+ *
+ * The consumer, which inherits from ProducerConsumer, implements consume().
+ * As tasks are produced, consume() is called for each task, one at a time.
+ *
+ * In single-threaded environments, produce() just calls consume().
+ */
+class ProducerConsumer
+{
+    public:
+        ProducerConsumer();
+        virtual ~ProducerConsumer();
+
+        bool initialise();
+
+        void produce(uint64_t p0 = 0, uint64_t p1 = 0, uint64_t p2 = 0, uint64_t p3 = 0,
+                     uint64_t p4 = 0, uint64_t p5 = 0, uint64_t p6 = 0, uint64_t p7 = 0,
+                     uint64_t p8 = 0);
+
+    private:
+        virtual void consume(uint64_t p0, uint64_t p1, uint64_t p2, uint64_t p3,
+                             uint64_t p4, uint64_t p5, uint64_t p6, uint64_t p7,
+                             uint64_t p8) = 0;
+
+        void consumerThread();
+
+        static int thread(void *p);
+
+        struct Task
+        {
+            uint64_t p0, p1, p2, p3, p4, p5, p6, p7, p8;
+        };
+
+#if PRODUCERCONSUMER_ASYNCHRONOUS
+        Mutex m_Lock;
+        ConditionVariable m_Condition;
+        List<Task *> m_Tasks;
+
+        void *m_pThreadHandle = nullptr;
+        bool m_Running = false;
+#endif
+};
+
+#endif
---
+++ src/system/kernel/utilities/ProducerConsumer.cc
@@ -0,0 +1,129 @@
+/*
+ * Copyright (c) 2008-2014, Pedigree Developers
+ *
+ * Please see the CONTRIB file in the root of the source tree for a full
+ * list of contributors.
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include "pedigree/kernel/utilities/ProducerConsumer.h"
+#include "pedigree/kernel/Log.h"
+
+#if PRODUCERCONSUMER_ASYNCHRONOUS
+#include "pedigree/kernel/utilities/pocketknife.h"
+#include "pedigree/kernel/LockGuard.h"
+#endif
+
+ProducerConsumer::ProducerConsumer() = default;
+
+ProducerConsumer::~ProducerConsumer()
+{
+#if PRODUCERCONSUMER_ASYNCHRONOUS
+    m_Lock.acquire();
+    if (m_Running)
+    {
+        m_Running = false;
+        m_Condition.signal();
+        m_Lock.release();
+
+        pocketknife::attachTo(m_pThreadHandle);
+    }
+    else
+    {
+        m_Lock.release();
+    }
+
+    // Clean up tasks that didn't get executed.
+    for (auto it : m_Tasks)
+    {
+        delete it;
+    }
+#endif
+}
+
+bool ProducerConsumer::initialise()
+{
+#if PRODUCERCONSUMER_ASYNCHRONOUS
+    LockGuard<Mutex> guard(m_Lock);
+
+    if (m_Running)
+    {
+        return true;
+    }
+
+    m_Running = true;
+    m_pThreadHandle = pocketknife::runConcurrentlyAttached(thread, this);
+    return m_pThreadHandle != nullptr;
+#else
+    return true;
+#endif
+}
+
+void ProducerConsumer::produce(uint64_t p0, uint64_t p1, uint64_t p2, uint64_t p3, uint64_t p4, uint64_t p5, uint64_t p6, uint64_t p7, uint64_t p8)
+{
+#if PRODUCERCONSUMER_ASYNCHRONOUS
+    Task *task = new Task;
+    task->p0 = p0;
+    task->p1 = p1;
+    task->p2 = p2;
+    task->p3 = p3;
+    task->p4 = p4;
+    task->p5 = p5;
+    task->p6 = p6;
+    task->p7 = p7;
+    task->p8 = p8;
+
+    m_Lock.acquire();
+    m_Tasks.pushBack(task);
+    m_Condition.signal();
+    m_Lock.release();
+#else
+    consume(p0, p1, p2, p3, p4, p5, p6, p7, p8);
+#endif
+}
+
+void ProducerConsumer::consumerThread()
+{
+    LockGuard<Mutex> guard(m_Lock);
+
+    while (m_Running)
+    {
+        while (!m_Condition.wait(m_Lock))
+            ;
+
+        if (!m_Running)
+        {
+            break;
+        }
+
+        Task *task = m_Tasks.popFront();
+
+        // Don't hold lock while we actually perform the consume operation.
+        m_Lock.release();
+
+        consume(task->p0, task->p1, task->p2, task->p3, task->p4, task->p5, task->p6, task->p7, task->p8);
+
+        delete task;
+
+        m_Lock.acquire();
+    }
+}
+
+int ProducerConsumer::thread(void *p)
+{
+    ProducerConsumer *pc = reinterpret_cast<ProducerConsumer *>(p);
+    pc->consumerThread();
+
+    return 0;
+}
