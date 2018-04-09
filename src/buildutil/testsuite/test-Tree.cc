e3b6b2ed6e2fae6b8cd28253b8eb29982fa38ed8
Matthew Iselin <matthew@theiselins.net>
2018-04-09 10:11:19 -0700

    testsuite: add tests for Tree

--- src/buildutil/testsuite/test-Tree.cc
+++ src/buildutil/testsuite/test-Tree.cc
@@ -22,3 +22,57 @@
 #include <gtest/gtest.h>
 
 #include "pedigree/kernel/utilities/Tree.h"
+
+TEST(PedigreeTree, Construction)
+{
+    Tree<int, int> x;
+    EXPECT_EQ(x.count(), 0);
+    EXPECT_EQ(x.begin(), x.end());
+}
+
+TEST(PedigreeTree, CopyConstruction)
+{
+    Tree<int, int> x;
+    x.insert(1, 1);
+
+    Tree<int, int> y(x);
+    EXPECT_EQ(x.count(), y.count());
+
+    EXPECT_EQ(x.lookup(1), y.lookup(1));
+}
+
+TEST(PedigreeTree, Clear)
+{
+    Tree<int, int> x;
+    // try and rebalance the tree a bit
+    for (int i = 0; i < 256; ++i)
+    {
+        x.insert(i, 1234);
+    }
+    for (int i = 512; i < 768; ++i)
+    {
+        x.insert(i, 1234);
+    }
+    for (int i = 256; i < 512; ++i)
+    {
+        x.insert(i, 1234);
+    }
+    for (int i = 768; i < 1024; ++i)
+    {
+        x.insert(i, 1234);
+    }
+    EXPECT_EQ(x.count(), 1024);
+    x.clear();
+    EXPECT_EQ(x.count(), 0);
+}
+
+TEST(PedigreeTree, Removal)
+{
+    Tree<int, int> x;
+    x.insert(1, 1);
+    x.insert(2, 2);
+    x.remove(1);
+    EXPECT_EQ(x.count(), 1);
+
+    EXPECT_EQ(x.lookup(2), 2);
+}
