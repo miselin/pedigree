ccea0bcec109e92669e02b7fc6aa9a296568a2db
Matthew Iselin <matthew@theiselins.net>
2018-05-02 11:32:34 -0700

    build: always build host tools in build-host even on test-only builds

--- easy_build_tests.sh
+++ easy_build_tests.sh
@@ -6,12 +6,12 @@
 set -e
 
 echo "Pedigree Easy Build"
-echo "NOTE: This Easy Build script only builds tools that run on your build " \
-    "system; this does not build Pedigree itself. Unless you are specifically" \
+echo "NOTE: This Easy Build script only builds tools that run on your build" \
+    " system; this does not build Pedigree itself. Unless you are specifically" \
     " working on tests or benchmarks, you probably want one of the other Easy" \
     " Build scripts."
 
-mkdir build && cd build
+mkdir build-host && cd build-host
 cmake ..
 
 make -j1
