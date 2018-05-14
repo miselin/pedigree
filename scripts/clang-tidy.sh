dac3e8054a4ed93f1e95b1a7530ce1507cf40f79
Matthew Iselin <matthew@theiselins.net>
2018-05-14 14:44:37 -0700

    scripts: update scripts/clang-tidy.sh with cmake instructions and add clang-tidy config file

--- scripts/clang-tidy.sh
+++ scripts/clang-tidy.sh
@@ -2,11 +2,13 @@
 
 # Run clang-tidy over the source tree.
 #
-# Requires you to have run bear with scons first:
-# $ bear scons
+# This requires a compilation database - build one with
+# -DCMAKE_EXPORT_COMPILE_COMMANDS=ON on the cmake command line.
+#
+# This is expected to be run from the root of the repository.
 
 echo >tidy.log
 if [ "x$SRCPATH" = "x" ]; then
     SRCPATH=src
 fi
-find $SRCPATH -regextype egrep -regex ".*\.(cc|c|cpp)$" -print0 | xargs -0 clang-tidy '-header-filter=.*' >>tidy.log
+find $SRCPATH -regextype egrep -regex ".*\.(cc|c|cpp)$" -print0 | xargs -0 clang-tidy -p build >>tidy.log
